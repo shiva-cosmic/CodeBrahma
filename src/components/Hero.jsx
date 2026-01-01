0fa98aade78217eba1d7a937e20cb4c3fcc0368e
Shivanshh369 <shivanshhh369@gmail.com>
2026-01-01 17:36:24 +0530

    fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -101,18 +101,6 @@
 
                     <ChevronDown className="w-4 h-4 text-gray-400" />
                   </div>
-                  <div className="flex items-center justify-between px-4 py-3 bg-black/20 border-b border-white/10">
-                    <div className="flex items-center space-x-3">
-                      <div className="flex gap-2">
-                        <div className="w-3 h-3 bg-red-500 rounded-full" />
-                        <div className="w-3 h-3 bg-yellow-400 rounded-full" />
-                        <div className="w-3 h-3 bg-green-500 rounded-full" />
-                      </div>
-                      <span className="text-gray-300 text-sm">Code Brahma</span>
-                    </div>
-
-                    <ChevronDown className="w-4 h-4 text-gray-400" />
-                  </div>
 
                   {/* Tabs style changed */}
                   <div className="flex space-x-3 px-4 py-2 bg-black/10 border-b border-white/5 overflow-x-auto">
